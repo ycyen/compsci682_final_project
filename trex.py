fc1c4b4f91365e3ab8b98586b3fcf331664ac8a6
Mark Yan <b01502069@ntu.edu.tw>
2018-11-08 16:49:29 -0500

    fix bug: rm sound, refactor

--- trex.py
+++ trex.py
@@ -1,37 +1,33 @@
-__author__ = "Shivam Shekhar"
-
 import os
 import sys
 import pygame
 import random
 from pygame import *
 
-display = False
-sound = False
 
-if not display:
+DISPLAY = False
+SUCCESS_REWARD = 5
+ALIVE_REWARD = 1
+JUMP_REWARD = -1
+DEAD_REWARD = -10
+
+
+if not DISPLAY:
     os.environ["SDL_VIDEODRIVER"] = "dummy"
 pygame.init()
 
 scr_size = (width,height) = (600,150)
-FPS = 99999999999999999999#30
+FPS = 99999999999999999999
 gravity = 0.6
 
 black = (0,0,0)
 white = (255,255,255)
 background_col = (235,235,235)
-# background_col = (0,0,0)
-
 high_score = 0
 
 screen = pygame.display.set_mode(scr_size)
 clock = pygame.time.Clock()
 pygame.display.set_caption("T-Rex Rush")
-
-if sound:
-    jump_sound = pygame.mixer.Sound('sprites/jump.wav')
-    die_sound = pygame.mixer.Sound('sprites/die.wav')
-    checkPoint_sound = pygame.mixer.Sound('sprites/checkPoint.wav')
 
 def load_image(
     name,
@@ -149,7 +145,6 @@ class Dino():
             self.isJumping = False
 
     def update(self):
-        global sound
         if self.isJumping:
             self.movement[1] = self.movement[1] + gravity
             # self.movement[1] = 0
@@ -186,10 +181,6 @@ class Dino():
 
         if not self.isDead and self.counter % 7 == 6 and self.isBlinking == False:
             self.score += 1
-            if self.score % 100 == 0 and self.score != 0:
-                if sound and pygame.mixer.get_init() != None:
-                    checkPoint_sound.play()
-
         self.counter = (self.counter + 1)
 
 class Cactus(pygame.sprite.Sprite):
@@ -303,60 +294,10 @@ class Scoreboard():
         self.temprect.left = 0
 
 
-def introscreen():
-    temp_dino = Dino(44,47)
-    temp_dino.isBlinking = True
-    gameStart = False
-
-    callout,callout_rect = load_image('call_out.png',196,45,-1)
-    callout_rect.left = width*0.05
-    callout_rect.top = height*0.4
-
-    temp_ground,temp_ground_rect = load_sprite_sheet('ground.png',15,1,-1,-1,-1)
-    temp_ground_rect.left = width/20
-    temp_ground_rect.bottom = height
-
-    logo,logo_rect = load_image('logo.png',240,40,-1)
-    logo_rect.centerx = width*0.6
-    logo_rect.centery = height*0.6
-    while not gameStart:
-        if pygame.display.get_surface() == None:
-            print("Couldn't load display surface")
-            return True
-        else:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return True
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
-                        temp_dino.isJumping = True
-                        temp_dino.isBlinking = False
-                        temp_dino.movement[1] = -1*temp_dino.jumpSpeed
-
-        temp_dino.update()
-
-        if pygame.display.get_surface() != None:
-            screen.fill(background_col)
-            screen.blit(temp_ground[0],temp_ground_rect)
-            if temp_dino.isBlinking:
-                screen.blit(logo,logo_rect)
-                screen.blit(callout,callout_rect)
-            temp_dino.draw()
-
-            pygame.display.update()
-
-        clock.tick(FPS)
-        if temp_dino.isJumping == False and temp_dino.isBlinking == False:
-            gameStart = True
-
-
 class GameState:
     def __init__(self):
         global high_score
         self.gamespeed = 4
-        startMenu = False
-        gameOver = False
-        gameQuit = False
         self.playerDino = Dino(44, 47)
         self.new_ground = Ground(-1 * self.gamespeed)
         self.scb = Scoreboard()
@@ -371,9 +312,6 @@ class GameState:
         Ptera.containers = self.pteras
         Cloud.containers = self.clouds
 
-        retbutton_image, retbutton_rect = load_image('replay_button.png', 35, 31, -1)
-        gameover_image, gameover_rect = load_image('game_over.png', 190, 11, -1)
-
         temp_images, temp_rect = load_sprite_sheet('numbers.png', 12, 1, 11, int(11 * 6 / 5), -1)
         HI_image = pygame.Surface((22, int(11 * 6 / 5)))
         HI_rect = HI_image.get_rect()
@@ -386,31 +324,25 @@ class GameState:
 
 
     def frame_step(self, input_actions, debug=False):
-        global high_score, sound
+        global high_score
         pygame.event.pump()
         terminal = False
         # if do nothing reward is 1
-        reward = 1
+        reward = ALIVE_REWARD
 
         # input_actions[0] == 1: do nothing
         # input_actions[1] == 1: press jump
         if input_actions[1] == 1:
             # if jump but not over obstacle
-            reward = -3
+            reward = JUMP_REWARD
             if self.playerDino.rect.bottom == int(0.98 * height):
                 self.playerDino.isJumping = True
-                if pygame.mixer.get_init() != None:
-                    if sound:
-                        jump_sound.play()
-                    self.playerDino.movement[1] = -1 * self.playerDino.jumpSpeed
-
+                self.playerDino.movement[1] = -1 * self.playerDino.jumpSpeed
 
         for c in self.cacti:
             c.movement[0] = -1*self.gamespeed
             if pygame.sprite.collide_mask(self.playerDino,c):
                 self.playerDino.isDead = True
-                if sound and pygame.mixer.get_init() != None:
-                    die_sound.play()
             else:
                 def success_jump(dino, cacti):
                     left, top, w, h = dino.rect
@@ -425,33 +357,30 @@ class GameState:
                     return False
 
                 if success_jump(self.playerDino, c):
-                    reward = 8
-
+                    reward = SUCCESS_REWARD
 
         # for p in self.pteras:
         #     p.movement[0] = -1*self.gamespeed
         #     if pygame.sprite.collide_mask(self.playerDino,p):
         #         self.playerDino.isDead = True
-        #         if sound and pygame.mixer.get_init() != None:
-        #             die_sound.play()
-
 
         # This block is for adding cacti
-        if len(self.cacti) < 1:
+        if len(self.cacti) < 2:
             if len(self.cacti) == 0:
                 self.cacti.empty()
                 self.cacti.add(Cactus(self.gamespeed,40,40))
-            # else:
-            #     for l in self.last_obstacle:
-            #         if l.rect.right < width*0.7 and random.randrange(0,50) == 10:
-            #             self.last_obstacle.empty()
-            #             self.last_obstacle.add(Cactus(self.gamespeed, 40, 40))
+            else:
+                for c in self.cacti:
+                    if c.rect.right < width*0.7 and random.randrange(0,50) == 10:
+                        # self.cacti.empty()
+                        self.cacti.add(Cactus(self.gamespeed, 40, 40))
 
         # For debug cacti
         if debug:
-            print("Dino: ", self.playerDino.rect)
-            for c in self.cacti:
-                print("cacti: ", c.rect)
+            pass
+            # print("Dino: ", self.playerDino.rect)
+            # for c in self.cacti:
+            #     print("cacti: ", c.rect)
 
         # if len(self.pteras) == 0 and random.randrange(0,200) == 10 and self.counter > 500:
         #     for l in self.last_obstacle:
@@ -477,9 +406,9 @@ class GameState:
             self.new_ground.draw()
             self.clouds.draw(screen)
             self.scb.draw()
-            if high_score != 0:
-                self.highsc.draw()
-                screen.blit(HI_image,HI_rect)
+            # if high_score != 0:
+            #     self.highsc.draw()
+            #     screen.blit(HI_image,HI_rect)
             self.cacti.draw(screen)
             # self.pteras.draw(screen)
             self.playerDino.draw()
@@ -490,13 +419,11 @@ class GameState:
         cur_score = self.playerDino.score
         # check if Dino dead
         if self.playerDino.isDead:
-            # gameOver = True
-            terminal = True
-            self.__init__()
-            # reward is -100 is dead
-            reward = -100
             if self.playerDino.score > high_score:
                 high_score = self.playerDino.score
+            terminal = True
+            self.__init__()
+            reward = DEAD_REWARD
 
         # This one will increase speed
         # if self.counter%700 == 699:
@@ -505,42 +432,14 @@ class GameState:
 
         self.counter = (self.counter + 1)
 
-        # while gameOver:
-        #     if pygame.display.get_surface() == None:
-        #         print("Couldn't load display surface")
-        #         gameQuit = True
-        #         gameOver = False
-        #     else:
-        #         for event in pygame.event.get():
-        #             if event.type == pygame.QUIT:
-        #                 gameQuit = True
-        #                 gameOver = False
-        #             if event.type == pygame.KEYDOWN:
-        #                 if event.key == pygame.K_ESCAPE:
-        #                     gameQuit = True
-        #                     gameOver = False
-        #
-        #                 if event.key == pygame.K_RETURN or event.key == pygame.K_SPACE:
-        #                     gameOver = False
-        #                     gameplay()
-        #     self.highsc.update(high_score)
-        #     if pygame.display.get_surface() != None:
-        #         disp_gameOver_msg(retbutton_image,gameover_image)
-        #         if high_score != 0:
-        #             self.highsc.draw()
-        #             screen.blit(HI_image,HI_rect)
-        #         pygame.display.update()
-        #     clock.tick(FPS)
-
-
         image_data = pygame.surfarray.array3d(pygame.display.get_surface())
         pygame.display.update()
         clock.tick(FPS)
         return image_data, reward, terminal, cur_score
 
+
 def gameplay():
     game_state = GameState()
-
     action = [0,0]
     action[0] = 1
     image_data, reward, terminal = game_state.frame_step(action)
@@ -556,40 +455,10 @@ def gameplay():
         action[0] = 1
         game_state.frame_step(action)
 
-    # startMenu = False
-    # gameOver = False
-    # gameQuit = False
-    #
-    #
-    # while not gameQuit:
-    #     while startMenu:
-    #         pass
-    #     while not gameOver:
-    #         for event in pygame.event.get():
-    #             if event.type == pygame.QUIT:
-    #                 gameQuit = True
-    #                 gameOver = True
-    #
-    #             if event.type == pygame.KEYDOWN: # press something
-    #                 if event.key == pygame.K_SPACE: # press space
-    #
-
-                    # if event.key == pygame.K_DOWN: # press down
-                    #     if not (playerDino.isJumping and playerDino.isDead):
-                    #         playerDino.isDucking = True
-
-                # if event.type == pygame.KEYUP:
-                #     if event.key == pygame.K_DOWN:
-                #         playerDino.isDucking = False
-
-    # pygame.quit()
-    # quit()
-
 
 def main():
-    # isGameQuit = introscreen()
-    # if not isGameQuit:
     gameplay()
+
 
 if __name__ == "__main__":
     main()

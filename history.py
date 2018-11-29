545d8eec7cf3a35dabb3c3985e86d29b0a57391d
Roger <roger@1x-nat-vl932-172-30-203-106.wireless.umass.edu>
2018-11-29 13:17:57 -0500

    modify drawing plot

--- history.py
+++ history.py
@@ -2,6 +2,12 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import argparse
+import sys
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dir', required=True, help="directory of folder")
 
 
 
@@ -9,25 +15,47 @@ import torch
 def load_pickle(file_path):
     with open(file_path, 'rb') as handle:
         unserialized_data = pickle.load(handle)
-    print(unserialized_data)
     return np.array(unserialized_data).flatten()
 
-def draw_loss(loss_hist):
+def merge(np1, np2, file_name): # merge two numpy array
+    print(np1.shape, np2.shape)
+    file = np.concatenate((np1, np2))
+    print(file.shape)
+    with open(file_name, 'wb') as handle:
+        pickle.dump(file, handle)
+
+def draw_loss(dir):
+    loss_hist = load_pickle(dir)
+
     plt.scatter(np.arange(len(loss_hist)), loss_hist)
     plt.xlabel("iteration")
     plt.ylabel("loss")
     plt.show()
 
-def draw_score(score_hist):
+def draw_score(dir):
+    score_hist = load_pickle(dir)
+
     plt.scatter(np.arange(len(score_hist)), score_hist)
     plt.xlabel("episode")
     plt.ylabel("score")
     plt.show()
 
 
+def main(argv):
+    args = parser.parse_args(argv)
+    ###### python3 history.py -dir FILE_DIRECTORY
+
+
+    ###### merge two history ######
+    # f1 = load_pickle(args.dir + 'loss_history_1.pickle')
+    # f2 = load_pickle(args.dir + 'loss_history_2.pickle')
+    # merge(f1, f2, args.dir + "loss_history.pickle")
+
+
+    ###### draw history ######
+    # draw_score(args.dir + "score_history.pickle")
+    draw_loss(args.dir + "loss_history.pickle")
+
 
 if __name__ == "__main__":
-    # loss_hist = load_pickle('loss_hist/loss_history_500.pickle')
-    # draw_loss(loss_hist)
-    score_hist = load_pickle('score_hist/score_history_600.pickle')
-    draw_score(score_hist)
+    main(sys.argv[1:])
